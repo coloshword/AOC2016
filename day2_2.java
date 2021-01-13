6f5a82eee1064a1b77734e983d64cff642c423bc
coloshword <aceliang2001@gmail.com>
2021-01-13 17:03:27 -0500

    day2 part 2 is done

--- day2_2.java
+++ day2_2.java
@@ -1,53 +1,49 @@
-import java.util.Scanner;
-import java.util.Arrays;
 import java.io.File;
+import java.util.Scanner;
+
+// start index is 2
 public class day2_2 {
     public static void main(String[] args) {
         try {
-            char[][] keypad = {
-                    {'1'}, {'2', '3', '4'}, {'5', '6', '7', '8', '9'}, {'A', 'B', 'C'}, {'D'}
-            };
             File fh = new File("day2.txt");
             Scanner input = new Scanner(fh);
-            int[] currentKey = {2, 0};
-            String key = "";
+            char[][] keypad = {{'0','0','1','0','0'}, {'0','2','3','4','0'}, {'5', '6', '7', '8', '9'}, {'0','A', 'B', 'C', '0'}, {'0', '0', 'D', '0', '0'}};
+            int[] keyLocation = {2, 0};
+            String ans = "";
             while (input.hasNextLine()) {
                 String instructions = input.nextLine();
-                currentKey = readInstructions(instructions, currentKey, keypad);
-                key += keypad[currentKey[0]][currentKey[1]];
+                keyLocation = readInstructions(instructions, keyLocation, keypad);
+                ans += keypad[keyLocation[0]][keyLocation[1]];
             }
-            System.out.println(key);
+            System.out.println(ans);
         }
-        catch(Exception FileNotFoundException) {
+        catch(Exception e) {
             System.out.println("Something went wrong");
         }
     }
-    public static int[] readInstructions(String instruct, int[] location, char[][] numPad) {
+
+    public static int[] readInstructions(String instruct, int[] start, char[][] keyPad) {
         for(int i = 0; i < instruct.length(); i++) {
             char movement = instruct.charAt(i);
             switch(movement) {
                 case 'U':
-                    if(location[0] > 0) {
-                        location[0] -= 1;
-                    }
+                    if(start[0] > 0 && keyPad[start[0] - 1][start[1]] != '0')
+                        start[0] -= 1;
                     break;
                 case 'D':
-                    if(location[0] < numPad.length - 1) {
-                        location[0] += 1;
-                    }
-                    break;
-                case 'L':
-                    if(location[1] > 0) {
-                        location[1] -= 1;
-                    }
+                    if(start[0] < 4 && keyPad[start[0] + 1][start[1]] != '0')
+                        start[0] += 1;
                     break;
                 case 'R':
-                    if(location[1] < numPad.length - 1) {
-                        location[1] += 1;
-                    }
+                    if(start[1] < 4 && keyPad[start[0]][start[1] + 1] != '0')
+                        start[1] += 1;
+                    break;
+                case 'L':
+                    if(start[1] > 0 && keyPad[start[0]][start[1] - 1] != '0')
+                        start[1] -= 1;
                     break;
             }
         }
-        return location;
+        return start;
     }
 }
