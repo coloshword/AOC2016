bd800b4ffe473fd0334b56a6e335f40c2b94f8d6
coloshword <aceliang2001@gmail.com>
2021-01-17 17:12:42 -0500

    day4 part 1 finished

--- day4.java
+++ day4.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.io.*;
 
 public class day4 {
@@ -10,11 +11,59 @@ public class day4 {
         while(input.hasNextLine()) {
             String line = input.nextLine();
             String key = line.substring(line.length() - 6, line.length() - 1);
-            if(isRoom(line, key)) {
+            if(isRoom2(line, key)) {
                 sum += Integer.parseInt(line.substring(line.length() - 10, line.length() - 7));
             }
         }
         System.out.println(sum);
+    }
+
+    public static boolean isRoom2(String room, String checkSum) {
+        int priorOccurrences = occurrences(checkSum.charAt(0), room);
+        HashMap<Character, Integer> map = new HashMap<>();
+        String uniqueChars = "";
+        for(int i = 0; i < room.length() - 11; i++) {
+            char currentChar = room.charAt(i);
+            if(!(contains(uniqueChars, currentChar)) && currentChar != '-') {
+                uniqueChars += currentChar;
+                map.put(currentChar, occurrences(currentChar, room));
+            }
+        }
+        System.out.println(map);
+        System.out.println(uniqueChars);
+        for(int i = 1; i < checkSum.length(); i++) {
+            char currentChar = checkSum.charAt(i);
+            int currentOccurrences = occurrences(currentChar, room);
+            if (currentOccurrences > priorOccurrences) {
+                return false;
+            }
+            if (currentOccurrences == priorOccurrences && ("" + checkSum.charAt(i - 1)).compareTo(currentChar + "") > 0) {
+                return false;
+            }
+            priorOccurrences = currentOccurrences;
+        }
+        // now check if no letters outside the checksum have a greater occurrence
+        for(int j = 0; j < checkSum.length(); j++) {
+            int currentMax = occurrences(checkSum.charAt(j), room);
+            for (int i = 0; i < uniqueChars.length(); i++) {
+                char currentChar = uniqueChars.charAt(i);
+                if (!contains(checkSum, currentChar)) {
+                    if(map.get(currentChar) > currentMax) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean contains(String a, char b) {
+        for(int i = 0; i < a.length(); i++) {
+            if(a.charAt(i) == b) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean isRoom(String room, String checkSum) {
