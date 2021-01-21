121f110b2055f5fb05c3a354de8f33da2ad2bdf7
coloshword <aceliang2001@gmail.com>
2021-01-20 23:23:26 -0500

    day6 part 2

--- day6.java
+++ day6.java
@@ -19,14 +19,18 @@ public class day6 {
         //System.out.println(chars);
         //now check for the most common occurence
         String ans = "";
+        String ans2 = "";
         for (int column = 0; column < charPerLine; column++) {
             HashMap<Character, Integer> map = new HashMap<>();
             for (int row = 0; row < chars.size(); row += charPerLine) {
                 updateMap(map, chars.get(column + row));
             }
             ans += getMaxValue(map);
+            ans2 += getMinValue(map);
         }
         System.out.println(ans);
+        // for part 2:
+        System.out.println(ans2);
     }
 
     public static void updateMap(HashMap<Character, Integer> a, char b) {
@@ -40,11 +44,27 @@ public class day6 {
     public static char getMaxValue(HashMap<Character, Integer> a) {
         int max = 0;
         char ans = 'x';
+        // for part 2
         for (Entry<Character, Integer> entry : a.entrySet()) {
             char key = entry.getKey();
             int value = entry.getValue();
             if(value > max) {
                 max = value;
+                ans = key;
+            }
+        }
+        return ans;
+    }
+
+    public static char getMinValue(HashMap<Character, Integer> a) {
+        int min = 100000;
+        char ans = 'x';
+        // for part 2
+        for (Entry<Character, Integer> entry : a.entrySet()) {
+            char key = entry.getKey();
+            int value = entry.getValue();
+            if(value < min) {
+                min = value;
                 ans = key;
             }
         }
